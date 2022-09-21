ebba786b70b73b794ab675cf2d3c579e5b51d629
luiznaiper <luiz_naiper@hotmail.com>
2022-09-21 07:38:47 -0500

    :construction: Start first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Container } from "@mui/material";
+import Searcher from "./componets";
 
 const App = () => {
   return(
-    <div>Soy un platzinauta</div>
+    <Container sx={{
+      background: '#2C2C38' ,
+      color: '#8c8c8e',
+      width: '80vh',
+      height: '400px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }}>
+        <Searcher/>
+    </Container>    
   )
 };
 
---
+++ src/componets/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {  Stack, TextField, IconButton, } from '@mui/material'
+
+const Searcher = () => {
+  return (
+    <Stack>
+        <TextField></TextField>
+        <IconButton></IconButton>
+    </Stack>
+  )
+}
+
+export default Searcher
