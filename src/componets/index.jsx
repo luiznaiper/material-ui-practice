a7d16252a6c655b29810af177766eab5acd2eb41
luiznaiper <luiz_naiper@hotmail.com>
2022-09-21 08:20:30 -0500

    :lipstick: Update Searcher styles

--- src/componets/index.jsx
+++ src/componets/index.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import {  Stack, TextField, IconButton, } from '@mui/material'
+import React from 'react';
+import { Stack, TextField, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Searcher = () => {
-  return (
-    <Stack>
-        <TextField></TextField>
-        <IconButton></IconButton>
-    </Stack>
-  )
-}
+  const stackStyles = {
+    marginTop: '30px',
+    width: '80%',
+  };
 
-export default Searcher
+  const textFieldStyles = {
+    width: '90%',
+    margin: '0 auto',
+    border: 'none',
+    background: 'white',
+  };
+
+  return (
+    <Stack sx={stackStyles} direction="row">
+      <TextField
+        sx={textFieldStyles}
+        type="search"
+        autoComplete="off"
+        label="Github User"
+        id="outlined-basic"
+        placeholder="Search user"
+        InputProps={{
+          endAdornment: (
+            <IconButton>
+              <SearchIcon />
+            </IconButton>
+          ),
+        }}
+        InputLabelProps={{
+          style: { color: '#8c8c8e' },
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default Searcher;
